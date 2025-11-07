2871056aaaf9a64172d4349a9ed849a3ded1b72a
Badia Abouanane <badiaabouanane@gmail.com>
2025-11-07 17:00:12 +0100

    Add Mongo connexion

--- src/config/mongoClient.js
+++ src/config/mongoClient.js
@@ -3,16 +3,25 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const uri = process.env.MONGO_URI; 
+const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
+let db;
 
 export async function connectToMongo() {
-    try {
-        await client.connect();
-        console.log('✅ Connecté à MongoDB');
-        return client.db('todolist');
-    } catch (err) {
-        console.error('❌ Erreur de connexion MongoDB :', err);
-        throw err;
-    }
+  if (!db) {
+    await client.connect();
+    db = client.db(process.env.MONGO_DB_NAME || 'exercice2');
+    console.log('Connecté à MongoDB');
+  }
+  return db;
 }
+
+export async function getMongoCollection(collectionName) {
+  const database = await connectToMongo();
+  return database.collection(collectionName);
+}
+
+ 
+
+
+
--- src/controllers/taskControllerMongo.js
+++ src/controllers/taskControllerMongo.js
@@ -1,11 +1,13 @@
 import Task from '../models/taskModelMongo.js';
+import { getMongoCollection } from '../config/mongoClient.js';  
 
 const taskModel = new Task();
 
 export default class TaskControllerMongo {
     async listTasks(req, res) {
         try {
-            const tasks = await taskModel.getAll();
+            const collection = await getMongoCollection('tasks');
+            const tasks = await collection.find({}).toArray();
             res.json(tasks);
         } catch (err) {
             console.error('Erreur lors de la récupération des tâches :', err);
@@ -16,18 +18,20 @@ export default class TaskControllerMongo {
     async addTask(req, res) {
         console.log('Requête reçue :', req.body);
         const { title, description } = req.body;
+        const collection = await getMongoCollection('tasks');
         if (!title || !description) {
             return res.status(400).json({ error: 'Title and description are required' });
         }
-        const newTask = await taskModel.addTask({ title, description });
+        const newTask = await collection.insertOne({ title, description });
         res.status(201).json(newTask);
     
     }
 
     async deleteTask(req, res) {
         const { id } = req.params;
-        const success = await taskModel.deleteTask(id);
-        if (!success) {
+        const collection = await getMongoCollection('tasks');
+        const result = await collection.deleteOne({ _id: id });
+        if (result.deletedCount === 0) {
             return res.status(404).json({ error: 'Task not found' });
         }
         res.status(204).send();
